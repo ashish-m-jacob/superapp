f5362ab4fe8ad9153462f8f9a9e893b507396f62
Ashish <ashishmjacob@gmail.com>
2025-04-08 17:32:37 +0530

    grid created for weather page

--- src/pages/Weather.jsx
+++ src/pages/Weather.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import styles from "./Weather.module.css";
 
 const Weather = () => {
-  return <div className={styles.pageContainer}></div>;
+  return (
+    <div className={styles.pageContainer}>
+      <div className={styles.leftDiv}>
+        <div className={styles.userInfo}></div>
+        <div className={styles.notes}></div>
+        <div className={styles.weatherInfo}></div>
+        <div className={styles.watchDetails}></div>
+      </div>
+      <div className={styles.rightDiv}>Right div</div>
+    </div>
+  );
 };
 
 export default Weather;
