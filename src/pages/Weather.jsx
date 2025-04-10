b9d13b111cf081c43cf28c8c06577827e966d647
Ashish M Jacob <83330583+ashish-m-jacob@users.noreply.github.com>
2025-04-10 16:42:07 +0530

    successful api call to openweather

--- src/pages/Weather.jsx
+++ src/pages/Weather.jsx
@@ -1,14 +1,75 @@
-import React from "react";
+import { React, useEffect, useState } from "react";
 import styles from "./Weather.module.css";
 
+const apiKey = "5da213236dff1af3152dfcfae5086ec7";
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Bangalore&appid=${apiKey}&units=metric`;
+
 const Weather = () => {
+  const [weatherData, setWeatherData] = useState([]);
+
+  const [requiredWeatherData, setRequiredWeatherData] = useState({
+    condition: "",
+    conditionIcon: "",
+    temperature: "",
+    mbarPressure: "",
+    mbarPressureIcon: "",
+    windSpeed: "",
+    windSpeedIcon: "",
+    humidity: "",
+    humidityIcon: "",
+  });
+
+  const [dates, setDates] = useState({
+    day: "",
+    month: "",
+    year: "",
+  });
+
+  const [time, setTime] = useState({
+    hours: "",
+    minutes: "",
+  });
+
+  //weather api call
+  useEffect(() => {
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => setWeatherData(data.weather))
+      .catch((error) => console.error("Error fetching weather data:", error));
+  }, []);
+
+  console.log(weatherData);
+
+  console.log(requiredWeatherData);
+
+  const [ampm, setAmpm] = useState("AM");
+
   const userInfo = localStorage.getItem("user");
   const user = JSON.parse(userInfo);
 
   const userGenres = localStorage.getItem("userGenre");
   const genres = JSON.parse(userGenres);
 
-  const date = new Date();
+  function getDate() {
+    const date = new Date();
+
+    setDates({
+      day: date.getDate(),
+      month: date.getMonth() + 1,
+      year: date.getFullYear(),
+    });
+
+    setTime({
+      hours: date.getHours(),
+      minutes: date.getMinutes(),
+    });
+
+    date.getHours() >= 12 ? setAmpm("PM") : setAmpm("AM");
+  }
+
+  setInterval(getDate, 1000);
+
+  console.log(dates);
 
   console.log(genres);
   return (
@@ -30,7 +91,21 @@ const Weather = () => {
           </div>
         </div>
         <div className={styles.notes}></div>
-        <div className={styles.weatherInfo}></div>
+        <div className={styles.weatherInfo}>
+          <div className={styles.timeDate}>
+            <span>
+              {dates.month}-{dates.day}-{dates.year}
+            </span>
+            <span>
+              {time.hours > 12 ? time.hours - 12 : time.hours}:
+              {time.minutes < 10 ? `0${time.minutes}` : time.minutes}
+              {ampm}
+            </span>
+          </div>
+          <div className={styles.weatherCond}></div>
+          <div className={styles.temperature}></div>
+          <div className={styles.windHumidity}></div>
+        </div>
         <div className={styles.watchDetails}></div>
       </div>
       <div className={styles.rightDiv}>Right div</div>
